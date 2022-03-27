a22f6199bb363c2e58b25c3704aed7d9113c1d00
Umeigbo Obumneme Steven <obumneme.umeigbo@decagon.dev>
2022-03-27 23:19:08 +0100

    constants

---
+++ src/main/java/com/example/logs/constant/Constants.java
@@ -0,0 +1,8 @@
+package com.example.logs.constant;
+
+public class Constants {
+
+    public static final String SIGNUP = "registration successful";
+    public static final String LOG_CREATED = "log created";
+
+}
